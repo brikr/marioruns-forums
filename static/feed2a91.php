9a83adbc38daf725847b24ee2191896d5cd903cf
cdodge1 <Conner_Dodge@intuit.com>
2019-10-11 10:46:25 -0600

    Changed external links to open in new tab each time

--- static/feed2a91.php
+++ static/feed2a91.php
@@ -18,7 +18,7 @@
 <title type="html"><![CDATA[Hi, I'm Christian]]></title>
 
 <content type="html" xml:base="http://marioruns.com/forum/viewtopic.php?t=14&amp;p=41#p41"><![CDATA[
-My name is Christian and I am 20 years old. I got into speed running this year and I decided to start with SM64 after watching <a href="http://www.twitch.tv/dazee_" class="postlink">daze's stream</a>. I play other games and I work a lot so my time to stay consistent with SM64 is limited. I'm currently practicing 16 star on VC.  <img src="http://marioruns.com/forum/images/smilies/icon_mrgreen.gif" alt=":mrgreen:" title="Mr. Green" /><p>Statistics: Posted by <a href="http://marioruns.com/forum/memberlist.php?mode=viewprofile&amp;u=60">dd2C</a> — 20 Nov 2013, 04:58</p><hr />
+My name is Christian and I am 20 years old. I got into speed running this year and I decided to start with SM64 after watching <a href="http://www.twitch.tv/dazee_" target="_blank" class="postlink">daze's stream</a>. I play other games and I work a lot so my time to stay consistent with SM64 is limited. I'm currently practicing 16 star on VC.  <img src="http://marioruns.com/forum/images/smilies/icon_mrgreen.gif" alt=":mrgreen:" title="Mr. Green" /><p>Statistics: Posted by <a href="http://marioruns.com/forum/memberlist.php?mode=viewprofile&amp;u=60">dd2C</a> — 20 Nov 2013, 04:58</p><hr />
 ]]></content>
 </entry>
 </feed>
--- static/feed3f1a.php
+++ static/feed3f1a.php
@@ -18,7 +18,7 @@
 <title type="html"><![CDATA[Task Completion]]></title>
 
 <content type="html" xml:base="http://marioruns.com/forum/viewtopic.php?t=149&amp;p=607#p607"><![CDATA[
-It seems our friend the mlg pro is starting up the task completion again, figured I'd share if any of you missed it and want to join in:<br /><br /><a href="http://www.reddit.com/r/speedrun/comments/1vzwwz/super_mario_64_task_competition_is_back/" class="postlink">http://www.reddit.com/r/speedrun/comments/1vzwwz/super_mario_64_task_competition_is_back/</a><p>Statistics: Posted by <a href="http://marioruns.com/forum/memberlist.php?mode=viewprofile&amp;u=98">trakilicious</a> — 24 Jan 2014, 10:56</p><hr />
+It seems our friend the mlg pro is starting up the task completion again, figured I'd share if any of you missed it and want to join in:<br /><br /><a href="http://www.reddit.com/r/speedrun/comments/1vzwwz/super_mario_64_task_competition_is_back/" target="_blank" class="postlink">http://www.reddit.com/r/speedrun/comments/1vzwwz/super_mario_64_task_competition_is_back/</a><p>Statistics: Posted by <a href="http://marioruns.com/forum/memberlist.php?mode=viewprofile&amp;u=98">trakilicious</a> — 24 Jan 2014, 10:56</p><hr />
 ]]></content>
 </entry>
 </feed>
